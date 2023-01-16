da909945559951d8bdff12a4c20a55180f89d418
Crowdbotics <engineering@crowdbotics.com>
2023-01-16 06:47:28 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tiny_moon_dev_57131/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tiny_moon_dev_57131;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "tiny_moon_dev_57131";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tiny-moon-dev-57131",
+  url: "https://tiny-moon-dev-57131.botics.co",
+  api: "https://tiny-moon-dev-57131.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
